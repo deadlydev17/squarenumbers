ce3ddaca3c4bf937ae426d6bfc697e5383a7e489
deadlydev17 <yusuftasliman@gmail.com>
2022-08-11 14:15:25 +0700

    Refactor all functions

--- main.cpp
+++ main.cpp
@@ -1,32 +1,31 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-#include <limits> //import std::numeric_limits
-#include <random> // std::mt19937, std::seed_seq, std::random_device, std::uniform_int_distribution
+#include <limits> // import std::numeric_limits
+#include <random> // import std::mt19937, std::seed_seq, std::random_device, std::uniform_int_distribution
 #include <vector> // import std::vector and std::vector::size_type
-
 
 // config for generate random number
 namespace Config {
-  constexpr int minNumber{2};
-  constexpr int maxNumber{4};
-}
+constexpr int minNumber{2};
+constexpr int maxNumber{4};
+} // namespace Config
 
-void ignoreLine(){
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+void ignoreLine() {
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 // get base number from user
-int getBaseSquareNumber(){
+int getBaseSquareNumber() {
   int baseNumber{};
   do {
     std::cout << "Start where? ";
     std::cin >> baseNumber;
-    if(!std::cin){
+    if (!std::cin) {
       std::cin.clear();
       ignoreLine();
     }
-  } while(baseNumber <= 0);
+  } while (baseNumber <= 0);
   return baseNumber;
 }
 
@@ -36,56 +35,106 @@ int getNumberToGenerate() {
   do {
     std::cout << "How many? ";
     std::cin >> num;
-    if(!std::cin){
+    if (!std::cin) {
       std::cin.clear();
       ignoreLine();
     }
-  } while(num <= 0);
+  } while (num <= 0);
   return num;
+}
+
+int getGues() {
+  while (true) {
+   int guess{};
+   std::cout << "> ";
+   std::cin >> guess;
+   if(!std::cin) {
+     std::cin.clear();
+     ignoreLine();
+   }else {
+    ignoreLine();
+    return guess;
+   }
+  }
 }
 
 // generate random number between 2 and 4
 namespace Random {
-  std::random_device rd;
-  std::seed_seq sq{rd(),rd(),rd(),rd(),rd(),rd(),rd(),rd()};
-  std::mt19937 mt{sq};
+std::random_device rd;
+std::seed_seq sq{rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
+std::mt19937 mt{sq};
 
-  int getRandomNumber(int min, int max){
-    std::uniform_int_distribution<> die{min, max};
-    return die(mt);
-  }
+int getRandomNumber(int min, int max) {
+  std::uniform_int_distribution<> die{min, max};
+  return die(mt);
 }
+} // namespace Random
 
 using list_num = std::vector<int>;
 using index_type = list_num::size_type;
 
-list_num getListNum(int baseNumber,int amountOfNumbers, int randomNumber) {
+list_num getListNum(int baseNumber, int totalNumbers, int randomNumber) {
   list_num listNums{};
-  listNums.resize(static_cast<index_type>(amountOfNumbers));
+  listNums.resize(static_cast<index_type>(totalNumbers));
   index_type lenght{static_cast<index_type>(std::size(listNums))};
-  for(index_type count{};count < lenght;++count) {
-    listNums[count] = (baseNumber) * (baseNumber) * randomNumber;
+  for (index_type count{}; count < lenght; ++count) {
+    listNums[count] = (baseNumber) * (baseNumber)*randomNumber;
     ++baseNumber;
   }
   return listNums;
 }
 
-bool playGame() {
+bool player(list_num numbers,int totalNumbers, int randomNumber) {
+  std::cout << "I generated " << totalNumbers << " square numbers. Do you know each number is after multiplying by " << randomNumber << '\n';
+  int count{0};
+  while (count < totalNumbers) {
+    int guess{getGues()};
+    auto found{std::find(numbers.begin(),numbers.end(),guess)};
+    --totalNumbers;
+    if (found == numbers.end())
+    {
+        auto num{std::min_element(numbers.begin(),numbers.end(),[](const auto& a,const auto& b){
+        return (a < b);
+        })};
+      std::cout << guess << " was not found\n";
+      std::cout << "try " << *num << " next time\n";
+      return true;
+    }else {
+      if (totalNumbers == 0) {
+        break;
+      }
+      std::cout << "Nice " << totalNumbers << " numbers left\n";
+      numbers.erase(found);
+    }
+  }
+    return false;
+}
 
+bool playGame() {
+  int baseNumber{getBaseSquareNumber()};
+  int totalNumbers{getNumberToGenerate()};
+  int randomNumber{Random::getRandomNumber(Config::minNumber, Config::maxNumber)};
+  list_num numbers{getListNum(baseNumber, totalNumbers, randomNumber)};
+  for(const auto& i:numbers) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+  if (player(numbers, totalNumbers, randomNumber))
+  {
+    return false;
+  }
+  return true;
 }
 
 int main() {
 
-  auto baseNumber{getBaseSquareNumber()};
-  auto amountOfNumbers{getNumberToGenerate()};
-  auto randomNumber{getNumberToGenerate()};
-
-  auto get{getListNum(baseNumber, amountOfNumbers,randomNumber)};
-
-  for(const auto& i:get) {
-    std::cout << i << ' ';
+  if (playGame())
+  {
+    std::cout << "You win\n";
+  }else
+  {
+    std::cout << "you lose\n";
   }
-  std::cout << '\n';
 
   return 0;
 }
